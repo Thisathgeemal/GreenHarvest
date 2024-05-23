5530abb48a1d2493b83d54a2d51c0d1848486aa0
Thisathgeemal <162775321+Thisathgeemal@users.noreply.github.com>
2024-05-23 06:00:33 +0530

    notification

--- script/registration.js
+++ script/registration.js
@@ -20,3 +20,20 @@ function submitForm(event) {
     event.preventDefault(); 
     swal("Success!", "Registration Successful!", "success");
 }
+
+// subscribe conform alert 
+document.querySelectorAll('.subscribeButton').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        if (!isUserLoggedIn()) {
+            swal("Error", "First, you have to log in to subscribe to this scheme!", "error");
+        } else {
+            swal("Success!", "You have successfully subscribed to the Scheme!", "success");
+        }
+    });
+});
+
+function isUserLoggedIn() {
+    return false; 
+}
+
